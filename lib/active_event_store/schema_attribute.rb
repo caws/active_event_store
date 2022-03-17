67514fb30dad2cd29393909d764855d937c3fac8
Charles Washington <charles_was_7@hotmail.com>
2022-03-17 19:42:01 -0300

    Add required attribute This will be used when dealing with hybrid-schemas.

--- lib/active_event_store/schema_attribute.rb
+++ lib/active_event_store/schema_attribute.rb
@@ -1,21 +1,25 @@
 module ActiveEventStore
   class SchemaAttribute
-    attr_accessor :attribute, :default_value
+    attr_accessor :attribute, :default_value, :required
 
-    def initialize(attribute:, default_value:)
+    def initialize(attribute:, default_value:, required: false)
       @attribute = attribute
       @default_value = default_value
+      @required = required
     end
 
     def ==(other)
-      attribute == other.attribute && default_value == other.default_value
+      attribute == other.attribute &&
+        default_value == other.default_value &&
+        required == other.required
     end
 
     class << self
-      def for(attribute:, default_value: nil)
+      def for(attribute:, default_value: nil, required: false)
         self.new(
           attribute: attribute,
-          default_value: default_value
+          default_value: default_value,
+          required: required
         )
       end
     end
--- spec/schema_attribute_spec.rb
+++ spec/schema_attribute_spec.rb
@@ -7,11 +7,13 @@ describe ActiveEventStore::SchemaAttribute do
     specify do
       schema_attribute = ActiveEventStore::SchemaAttribute.for(
         attribute: :some_attribute_name,
-        default_value: 'some_default_value'
+        default_value: 'some_default_value',
+        required: false
       )
 
       expect(schema_attribute.attribute).to eq(:some_attribute_name)
       expect(schema_attribute.default_value).to eq('some_default_value')
+      expect(schema_attribute.required).to eq(false)
     end
   end
 
@@ -19,14 +21,16 @@ describe ActiveEventStore::SchemaAttribute do
     let(:schema_attribute) {
       ActiveEventStore::SchemaAttribute.for(
         attribute: :some_attribute_name,
-        default_value: 'some_default_value'
+        default_value: 'some_default_value',
+        required: false
       )
     }
 
     specify "should be equal" do
       new_schema_attribute = ActiveEventStore::SchemaAttribute.for(
         attribute: :some_attribute_name,
-        default_value: 'some_default_value'
+        default_value: 'some_default_value',
+        required: false
       )
 
       expect(schema_attribute).to eq(new_schema_attribute)
