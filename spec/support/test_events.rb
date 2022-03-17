ec55995a5531f95e1d26506fce6a4cdfb37c3d53
Charles Washington <charles_was_7@hotmail.com>
2022-03-17 18:53:34 -0300

    Setup test event with schema

--- spec/support/test_events.rb
+++ spec/support/test_events.rb
@@ -11,4 +11,18 @@ module ActiveEventStore
 
   class AnotherTestEvent < TestEvent
   end
+
+  class TestEventSchema < ActiveEventStore::Schema
+    attribute :user_id, 'some_default_value_for_user_id'
+    attribute :action_type, 'some_default_value_for_action_type'
+    attribute :some_boolean, true
+  end
+
+  class TestEventWithSchema  < ActiveEventStore::Event
+    self.identifier = "test_event_with_schema"
+
+    with_schema TestEventSchema
+
+    sync_attributes :user
+  end
 end
