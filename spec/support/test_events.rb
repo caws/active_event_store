f6d5d268a42e67ba66627bffd0256f67c258748c
Charles Washington <charles_was_7@hotmail.com>
2022-03-17 19:39:07 -0300

    Add test event with hybrid schema

--- spec/support/test_events.rb
+++ spec/support/test_events.rb
@@ -25,4 +25,18 @@ module ActiveEventStore
 
     sync_attributes :user
   end
+
+  class TestEventHybridSchema < ActiveEventStore::Schema
+    attribute :user_id, 'some_default_value_for_user_id', required: true
+    attribute :action_type, 'some_default_value_for_action_type'
+    attribute :some_boolean, true
+  end
+
+  class TestEventWithHybridSchema  < ActiveEventStore::Event
+    self.identifier = "test_event_with_hybrid_schema"
+
+    with_schema TestEventHybridSchema
+
+    sync_attributes :user
+  end
 end
